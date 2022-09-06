a394674083f077fef77b54980db7feb75990f2f1
Kaja <kathleengracenoble@gmail.com>
2022-09-06 10:37:41 -0600

    form fills from ocr_clean

--- apps/web/pages/add_image.py
+++ apps/web/pages/add_image.py
@@ -84,10 +84,14 @@ def basic_clean(ocr_dict:dict):
         ocr_dict[key] = s2
         if not key == 'date':
             ocr_dict[key]=ocr_dict[key].replace("A", '4')
-        print(ocr_dict)
     return ocr_dict
 
-clean_ocr = basic_clean(extract_data(snip(preprocess(image))))
+
+### Empty forms 
+empty_single_table = {'Date':[],'Time':[],'Distance':[],'Split':[],'s/m':[],'HR':[],'Comment':[]}
+empty_post_wo_dict = {'user_id':None, 'workout_date':None,'time_sec':None,'distance':None,'split':None,'sr':None,'hr':None,'intervals':1, 'comment':None}
+empty_intrvl_table = {'Date':[],'Time':[],'Distance':[],'Split':[],'s/m':[],'HR':[],'Rest':[],'Comment':[]}
+empty_post_intrvl_dict = {'workout_id':None,'time_sec':None,'distance':None,'split':None,'sr':None,'hr':None,'rest':None,'comment':None}
 
 ## TEST CONNECT
 
@@ -97,26 +101,77 @@ def layout(user_id=1):
         dcc.Markdown('# Add Workout'),
         dcc.RadioItems(options=['Single Time/Distance','Intervals'], value='Single Time/Distance', id='radio_select'),
         dbc.Row([
+        # Upload + Display image
             dbc.Col([
                 dcc.Upload(
                     id='upload-image',
                     children=html.Button('Upload Image', id="upload_image")),
+                dcc.Store(id='base64_img', data=None),
+                dcc.Store(id='np_array_img', data=None),
                 dcc.Store(id='raw_ocr', data=None),
                 html.Div(id='output-upload')]),
+        # data form feilds
             dbc.Col([
                 dcc.Store(id='ocr_dict', data=None),
-                dcc.Markdown('second col')
-        ])])])
+                dbc.Row([
+                    dbc.Col(dbc.Label('Date',html_for='ui_date2'), width=2),
+                    dbc.Col(dcc.Input(placeholder='yyyy-mm-dd',id="ui_date2", size='20' ), width=4)
+                    ]),
+                dbc.Row([
+                    dbc.Col(dbc.Label('Time',html_for='ui_time2'), width=2),
+                    dbc.Col(dcc.Input(placeholder='hh:mm:ss.t', id='ui_time2', size='15', maxLength=12), width=4)
+                    # dbc.Col(dbc.Label('Time',html_for='ui_hours2'), width=2),
+                    # dbc.Col(dcc.Input(placeholder='hours', id='ui_hours2', size='10', maxLength=2), width=2),
+                    # dbc.Col(dcc.Input(placeholder='minutes',id='ui_min2',size='10', maxLength=2), width=2),
+                    # dbc.Col(dcc.Input(placeholder='seconds',id='ui_sec2',size='10', maxLength=2), width=2),
+                    # dbc.Col(dcc.Input(placeholder='tenths',id='ui_ten2',size='10', maxLength=1), width=2)
+                    ]),
+                dbc.Row([
+                    dbc.Col(dbc.Label('Distance',html_for='ui_dist2'), width=2),
+                    dbc.Col(dcc.Input(placeholder='meters',id="ui_dist2", size='10'), width=4)
+                    ]),
+                dbc.Row([
+                    dbc.Col(dbc.Label('Split',html_for='ui_split2'), width=2),
+                    dbc.Col(dcc.Input(placeholder='m:ss.d',id="ui_split2", size='10'), width=4)
+                    ]),
+                dbc.Row([
+                    dbc.Col(dbc.Label('Stroke Rate',html_for='ui_sr2'), width=2),
+                    dbc.Col(dcc.Input(placeholder='s/m',id="ui_sr2", size='10'), width=4)
+                    ]),
+                dbc.Row([
+                    dbc.Col(dbc.Label('Heart Rate',html_for='ui_hr2'), width=2),
+                    dbc.Col(dcc.Input(placeholder='BPM',id="ui_hr2", size='10'), width=4)
+                    ]),
+                dbc.Row([
+                    dbc.Col(dbc.Label('Comment',html_for='ui_com2'), width=2),
+                    dbc.Col(dcc.Input('',id="ui_com2", size='20'), width=8)
+                    ]),
+                dbc.Row([
+                    dbc.Col(dbc.Label('Rest',html_for='ui_rest2'), width=2),
+                    dbc.Col(dcc.Input(placeholder='seconds',id="ui_rest2", size='10'), width=4)
+                    ]),
+                dbc.Button('Submit Interval', id='interval_submit', n_clicks=0, color='primary'),
+                dbc.Alert(id='intrvl_alert', style={'display':'none'},color='warning'),
+                dcc.Store(id='int_dict', data=empty_intrvl_table),
+                dcc.Store(id='intrvl_formatting_approved', data=False),
+                dbc.Row(
+                    [dbc.Table.from_dataframe(pd.DataFrame(empty_intrvl_table),striped=True,bordered=True)], 
+                    id='interval_table'),
+                dbc.Button('Submit workout', id='intwo_submit', n_clicks=0, color='primary')
+                ], id='int_pg')
+        ])])
 
+#upload pic
 @callback(
     Output('output-upload', 'children'),
-    Output('raw_ocr', 'data'),
+    Output('base64_img', 'data'),
     Input('upload-image', 'contents'),
     State('upload-image', 'filename'),
     State('upload-image', 'last_modified'))
-def update_output(contents, filename, date):
+def upload_img(contents, filename, date):
     if contents is not None:
-        data = contents
+        base64_img = contents[23:]
+        print('b64: ', type(base64_img))
         children = dbc.Container([
             html.H5(filename),
             html.H6(datetime.datetime.fromtimestamp(date)),
@@ -130,7 +185,65 @@ def update_output(contents, filename, date):
                 'wordBreak': 'break-all'
             })
         ])
-        return children, data
+        return children, base64_img
+
+
+@callback(
+    Output('np_array_img', 'data'),
+    Input('base64_img', 'data')
+)
+def convert_to_cv2_compatible(b64):
+    if b64 is not None:
+        print('run convert to cv2 compatible')
+        b64_bytes = b64.encode('ascii')
+        im_bytes = base64.b64decode(b64_bytes)
+        print('bytes?', type(im_bytes))
+        im_arr = np.frombuffer(im_bytes, dtype=np.uint8) # im_arr is one-dim np array
+        img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+        print('img', type(img))
+        return img
+
+
+@callback(
+    Output('raw_ocr', 'data'),
+    Input('np_array_img', 'data')
+)
+def extract_ocr(image):
+    if image is not None: 
+        image = np.array(image, dtype='uint8')
+        # image = np.uint8(image) WHAT IS A DICT?
+        img = preprocess(image)
+        snips:dict = snip(img)
+        ocr_dict:dict = extract_data(snips)
+        clean_ocr:dict = basic_clean(ocr_dict)
+        # clean_ocr = basic_clean(extract_data(snip(preprocess(image))))
+        for k in clean_ocr:
+            if k != 'date':
+                clean_ocr[k] = clean_ocr[k].split()
+        print(clean_ocr)
+        return clean_ocr 
+
+
+@callback(
+    Output('ui_date2', 'value'),
+    Output('ui_time2', 'value'),
+    # Output('ui_hours2', 'value'),
+    # Output('ui_min2', 'value'),
+    # Output('ui_sec2', 'value'),
+    # Output('ui_ten2', 'value'),
+    Output('ui_dist2', 'value'),
+    Output('ui_split2', 'value'),
+    Output('ui_sr2', 'value'),
+    Output('ui_hr2', 'value'),
+    Input('raw_ocr', 'data'),
+    State('interval_submit', 'n_clicks')
+)
+def fill_form(raw_ocr, n_clicks):
+    if raw_ocr is not None and n_clicks==0: 
+        hr = 'n/a'
+        if len(raw_ocr['summary']) == 5:
+            hr = raw_ocr['summary'][4] 
+        return raw_ocr['date'], raw_ocr['summary'][0], raw_ocr['summary'][1], raw_ocr['summary'][2], raw_ocr['summary'][3], hr
 
 
 ## END TUTORIAL
